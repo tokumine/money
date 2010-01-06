b0fe101127a1b0e28fadf6d81073cd6aa58d08d5
masa <masayuki@japancentre.com>
2010-01-06 17:01:39 +0000

    Fixed bug

--- lib/support/cattr_accessor.rb
+++ lib/support/cattr_accessor.rb
@@ -1,4 +1,4 @@
-# Extends the class object with class and instance accessors for class attributes, 
+# Extends the class object with class and instance accessors for class attributes,
 # just like the native attr* accessors for instance attributes.
 class Class # :nodoc:
   def cattr_reader(*syms)
@@ -11,22 +11,22 @@ class Class # :nodoc:
         def self.#{sym.id2name}
           @@#{sym}
         end
-
+ 
         def #{sym.id2name}
           @@#{sym}
         end
-
+ 
         def call_#{sym.id2name}
           case @@#{sym.id2name}
-            when Symbol then send(@@#{sym})
-            when Proc   then @@#{sym}.call(self)
-            when String then @@#{sym}
-            else nil
+          when Symbol then send(@@#{sym})
+          when Proc then @@#{sym}.call(self)
+          when String then @@#{sym}
+          else nil
           end
         end
       EOS
     end
-  end
+  end unless instance_methods.include?('cattr_reader')
   
   def cattr_writer(*syms)
     syms.each do |sym|
@@ -34,24 +34,24 @@ class Class # :nodoc:
         if ! defined? @@#{sym.id2name}
           @@#{sym.id2name} = nil
         end
-        
+
         def self.#{sym.id2name}=(obj)
           @@#{sym.id2name} = obj
         end
-
+ 
         def self.set_#{sym.id2name}(obj)
           @@#{sym.id2name} = obj
         end
-
+ 
         def #{sym.id2name}=(obj)
           @@#{sym} = obj
         end
       EOS
     end
-  end
+  end unless instance_methods.include?('cattr_writer')
   
   def cattr_accessor(*syms)
     cattr_reader(*syms)
     cattr_writer(*syms)
-  end
+  end unless instance_methods.include?('cattr_accessor')
 end
